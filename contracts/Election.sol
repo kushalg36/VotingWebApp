b5884989214290b65ab632b51a921300c473805c
kushalg36 <halkusg@gmail.com>
2018-08-05 17:43:44 +0530

    multiple candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,24 @@
 pragma solidity ^0.4.24;
 
 contract Election {
-  string public candidate;
-  /* Constructor class for storing and reading candidate name */
+  /* Model a Candidate */
+  struct Candidate {
+    uint id;
+    string name;
+    uint voteCount;
+  }
+  /* Store a Candidate*/
+  mapping(uint => Candidate) public candidates;
+  /* Storing the number of candidates */
+  uint public candidatesCount;
+  /* Constructor */
   function Election() public {
-    candidate = "Candidate 1";
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
+  }
+
+  function addCandidate (string _name) private {
+    candidatesCount ++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
   }
 }
