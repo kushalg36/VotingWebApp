6d67f8c762f00e6947f6f14b387cb04691e1c598
kushalg36 <halkusg@gmail.com>
2018-08-05 18:42:45 +0530

    Candidate choose

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -7,6 +7,8 @@ contract Election {
     string name;
     uint voteCount;
   }
+  /* Store accounts */
+  mapping(address => bool)public voters;
   /* Store a Candidate*/
   mapping(uint => Candidate) public candidates;
   /* Storing the number of candidates */
@@ -20,5 +22,16 @@ contract Election {
   function addCandidate (string _name) private {
     candidatesCount ++;
     candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+  }
+
+  function vote (uint _candidateId) public {
+    /* Condition for only vote by address */
+    require(!voters[msg.sender]);
+    /* Condition to check id lies between 0 and number of candidates */
+    require(_candidateId > 0 && _candidateId <= candidatesCount);
+    /* record that voter has voted */
+    voters[msg.sender] = true;
+    /* update candidate vote count */
+    candidates[_candidateId].voteCount ++;
   }
 }
